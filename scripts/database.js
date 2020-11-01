78219be0ef4378cec593c520af5fbef37041931e
Juliane <juliane.dominique.huber@gmail.com>
2020-11-01 14:34:53 +0100

    Added the dummy database using local storage

---
+++ scripts/database.js
@@ -0,0 +1,37 @@
+"use strict"
+
+class Database {
+  // a method to get users *array* from the localStorage 
+  getAllUsers =() => {
+    //key to look for in local storage - it's a string 
+      const usersStr = localStorage.getItem("users");
+    // convert the string to an array 
+      const usersArray = JSON.parse(usersStr); 
+    if (usersStr === null){
+    // if no users has yet signed up, the string would be empty (ls empty)
+        return [];
+    }
+    else {
+      return usersArray;
+    }
+  }
+    // a method to save a user into the localStorage users array
+    // careful user data comes from the data entered on the form - class User, instance new User (object)
+    saveNewUser = (newUser) => {
+    // get the array of users saved in the localStorage 
+    const usersArray = this.getAllUsers(); 
+
+    //update the users array and add the new user to it
+    const updatedUsersArr = [...usersArray, newUser]
+
+    // save back the updated users array to local storage
+    const updatedUsersStr = JSON.stringify(updatedUsersArr); 
+    localStorage.setItem("users", updatedUsersStr); 
+}
+}
+// create an instance for the db
+const db = new Database(); 
+
+console.log("db", db);
+
+//QUESION - why arrow functions 
--- scripts/signup.js
+++ scripts/signup.js
@@ -1,57 +1,53 @@
-"use strict"; 
-//good practice 
-//Sign up class will be interacting with the input fields 
-// Get all the elements that are typed into the input boxes 
-// only handles sign up functionality --> sepearation of concerns 
+"use strict";
+//good practice
+//Sign up class will be interacting with the input fields
+// Get all the elements that are typed into the input boxes
+// only handles sign up functionality --> sepearation of concerns
 
 class Signup {
-    constructor() {
-    // store all *input* elements 
-    this.fnameInput = document.querySelector("#fname"); 
+  constructor() {
+    // store all *input* elements
+    this.fnameInput = document.querySelector("#fname");
     this.lnameInput = document.querySelector("#lname");
-    this.emailInput = document.querySelector("#email"); 
+    this.emailInput = document.querySelector("#email");
     this.passwordInput = document.querySelector("#pword");
-    this.passwordRepeatInput = document.querySelector("#repeat-pword"); 
-    // access *button* 
+    this.passwordRepeatInput = document.querySelector("#repeat-pword");
+    // access *button*
     this.buttonInput = document.querySelector("#sign-up-button");
     // to display *error messages* we have to access the div element
-    this.errorMessages = document.querySelector(".error-messages"); 
-    }
+    this.errorMessages = document.querySelector(".error-messages");
+  }
 
-    // all functions interacting with the sign up form are put into the constructor 
-    // handle the input of the email: read the input and evaluate it 
+  // all functions interacting with the sign up form are put into the constructor
+  // handle the input of the email: read the input and evaluate it
 
-    handleEmailInput = (e) => {
-        // important - event object is the first parameter coming from the addeventlistener provided by the browser, it has information about the event
-        //console.log(e); 
-        // e.target!! gets the element <input ...>, value is inside of it 
-        const email = e.target.value;
-        //console.log(email);
-    }
-    // hanlde the input of the password 
-    handlePasswordInput = (e) => {
-        const password = e.target.value; 
-        //console.log(password);
-    }
-
-    // handle the repeat password 
-    handleRepeatPasswordInput = (e) => {
-        const repeatPassword = e.target.value;
-        //console.log(repeatPassword);
-    }
-
-    // save the data once submitted (button clicked)
-    saveData = (e) => {
-    
-
-    // prevent the default behavior of the submit button 
+  handleEmailInput = (e) => {
+    // important - event object is the first parameter coming from the addeventlistener provided by the browser, it has information about the event
+    //console.log(e);
+    // e.target!! gets the element <input ...>, value is inside of it
+    const email = e.target.value;
+    //console.log(email);
+  };
+  // hanlde the input of the password
+  handlePasswordInput = (e) => {
+    const password = e.target.value;
+    //console.log(password);
+  };
+  // handle the repeat password
+  handleRepeatPasswordInput = (e) => {
+    const repeatPassword = e.target.value;
+    //console.log(repeatPassword);
+  };
+  // save the data once submitted (button clicked)
+  saveData = (e) => {
+    // prevent the default behavior of the submit button
     // calling prevent default on the event object
     e.preventDefault();
     // get the value of the inputs; this fnameInput is the element and value the "input"
-        const fName = this.fnameInput.value;
-        const lName = this.lnameInput.value; 
-        const email = this.emailInput.value; 
-        const password = this.passwordInput.value; 
+    const fName = this.fnameInput.value;
+    const lName = this.lnameInput.value;
+    const email = this.emailInput.value;
+    const password = this.passwordInput.value;
     // create new user as an object literal
     /*const newUser = {
             firstName: fName, 
@@ -60,48 +56,42 @@ class Signup {
             password: password,
         }
     */
-    const newUser = new User (fName, lName, email, password); 
+    const newUser = new User(fName, lName, email, password);
     console.log(newUser);
     // save the user in the database
-    // empty the form 
-        this.fnameInput.value = "";
-        this.lnameInput.value = "";
-        this.emailInput.value = "";
-        this.passwordInput.value = "";
-        this.passwordRepeatInput.value = "";
-    }
-    // add Event Listeners 
-    // input event fires when the value of an <input>, <select> or <textarea> has been changed
-    addListeners = () => {
-        this.emailInput.addEventListener("input", this.handleEmailInput);
-        // whenever user enters/inputs (even just one key) something, handle EmailInput is called. When it is run the browser provides the event object!!
-        // Important --> the first parameter for the function is the event object, type depends on the specified event, click belongs to the MouseEvent object 
-        this.passwordInput.addEventListener("input", this.handlePasswordInput);
-        this.passwordRepeatInput.addEventListener("input", this.handleRepeatPasswordInput); 
-        // once clicked as submitted, save data is called, runs once and creates user from that
-        this.buttonInput.addEventListener("click", this.saveData); 
-    }
+    db.saveNewUser(newUser);
+    // empty the form
+    this.fnameInput.value = "";
+    this.lnameInput.value = "";
+    this.emailInput.value = "";
+    this.passwordInput.value = "";
+    this.passwordRepeatInput.value = "";
+  };
+  // add Event Listeners
+  // input event fires when the value of an <input>, <select> or <textarea> has been changed
+  addListeners = () => {
+    this.emailInput.addEventListener("input", this.handleEmailInput);
+    // whenever user enters/inputs (even just one key) something, handle EmailInput is called. When it is run the browser provides the event object!!
+    // Important --> the first parameter for the function is the event object, type depends on the specified event, click belongs to the MouseEvent object
+    this.passwordInput.addEventListener("input", this.handlePasswordInput);
+    this.passwordRepeatInput.addEventListener(
+      "input",
+      this.handleRepeatPasswordInput
+    );
+    // once clicked as submitted, save data is called, runs once and creates user from that
+    this.buttonInput.addEventListener("click", this.saveData);
+  };
 }
 
 // creating *one* instance - class is just to organize the code, singletone pattern
 // object is created
-// have the properties defined in the class plus methods 
+// have the properties defined in the class plus methods
 const signup = new Signup();
-console.log(Signup); // class 
+console.log(Signup); // class
 console.log(signup); // output the object with properties and methods, e.g. email Input: input#email
 
-// add eventlisteners when everything (files, resources) are loaded 
-// important as we need other files to be loaded, e.g. validator.js, meaning the 
+// add eventlisteners when everything (files, resources) are loaded
+// important as we need other files to be loaded, e.g. validator.js, meaning the
 //input is handled by the class sign up, validated
 // by validator.js class
 window.addEventListener("load", signup.addListeners);
-
-
-
-
-
-
-
-
-
-
