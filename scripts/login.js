fa3c2a8b05bcd3cde764bbeb81c783345477767c
Juliane <juliane.dominique.huber@gmail.com>
2020-11-01 18:40:24 +0100

    Started working on the login functionality

--- scripts/login.js
+++ scripts/login.js
@@ -1,1 +1,16 @@
 "use strict";
+
+class LogIn {
+    constructur () {
+    // accessing input fields 
+    this.firstName = document.querySelector("#fname");
+    this.emailInput = document.querySelector("#email");
+    this.passwordInput = document.querySelector("#password");
+    //accessing button login 
+    this.logInButton = document.querySelector("#log-in-button"); 
+    // accessing error messages 
+    this.errorMessages = document.querySelector(".error-messages")
+    }
+
+
+}
