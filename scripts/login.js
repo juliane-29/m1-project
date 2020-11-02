b12021bbb1bde1179a0166d6ea32c7a98d9234be
Juliane <juliane.dominique.huber@gmail.com>
2020-11-02 12:34:56 +0100

    Finished login functionality

--- scripts/login.js
+++ scripts/login.js
@@ -13,37 +13,49 @@ class Login {
   }
   // once the user has submitted both email and password
   handleSubmit = (e) => {
-  // prevent defaul reloading once clicked
-  e.preventDefault(); 
-  // get the values from the inputs 
-    const email = this.emailInput.value;  
+    // prevent defaul reloading once clicked
+    e.preventDefault();
+    // get the values from the inputs
+    const email = this.emailInput.value;
     const password = this.passwordInput.value;
 
-  // get the users from the db, we have to call the function in the db that get all user data 
-  // on the instance 
-  // variable saving all users, compare email with the db 
-  const allUsers = db.getAllUsers();
-  allUsers.find(function(userObj){
-      if (userObj.email === email && userObj.password === password){
-      return true;
+    // get the users from the db, we have to call the function in the db that get all user data
+    // on the instance
+    // variable saving all users, compare email with the db
+    // [{user1, 456}{ user2, 567}{ user3, 8910}]
+    // arr.find() - returns the first element that matches the expression
+    // check the pw and email exist in the db
+    // is email equal the input email? NO! keeps on going until true --> once true, the loop stops and return
+    const allUsers = db.getAllUsers();
+    const user = allUsers.find(function (userObj) {
+      if (userObj.email === email && userObj.password === password) {
+        return true;
       }
-  })
-  // arr.find() - returns the first element that matches the expression
-  // check the pw and email exist in the db 
-}
+    });
+    const p = document.createElement("p");
+    this.errorMessages.innerHTML = "";
+
+    if (!user) {
+      // user is not found as in email and pw are incorrect
+      p.textContent = "Email or Password are incorrect";
+    } else {
+      // user has been found in db
+      p.textContent = `Hello ${user.firstName}`;
+    }
+
+    this.errorMessages.appendChild(p);
+  };
 }
 // create an instance
 const login = new Login();
-// it does not show the properties though? typo!! 
-// checking properties and methods of the class 
+// it does not show the properties though? typo!!
+// checking properties and methods of the class
 
 console.log(login);
 
 // only one eventListener as we do not check the input
 // the page + ressources must be loaded
-window.addEventListener('load', function () {
-
-    login.loginButton.addEventListener('click', login.handleSubmit );
-  
-  } )
-  // handleSubmit function --> first paramenter is the event 
+window.addEventListener("load", function () {
+  login.loginButton.addEventListener("click", login.handleSubmit);
+});
+// handleSubmit function --> remember: first paramenter is the event
